3668d3ebf980a2dad104646c6551153ab16f15e0
Edu <eduardoyauri@emmake.com>
2023-09-16 12:01:57 +0200

    Test 3

--- functions.js
+++ functions.js
@@ -2,16 +2,15 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     document.addEventListener("keydown", function(event) {
-      // Verificar si se presionó la tecla "1"
+      
       if (event.key === "1") {
-        // Encontrar la sección "whoweare" por su id
+        
         const whoweareSection = document.getElementById("whoweare");
         
         if (whoweareSection) {
-          // Obtener la posición superior de la sección "whoweare"
+          
           const whoweareTop = whoweareSection.getBoundingClientRect().top;
           
-          // Desplazar la página hasta la posición de la sección "whoweare"
           window.scrollTo({
             top: whoweareTop,
             behavior: "smooth"
@@ -34,6 +33,66 @@ document.addEventListener("DOMContentLoaded", function() {
           
           window.scrollTo({
             top: serviceTop,
+            behavior: "smooth"
+          });
+        }
+      }
+    });
+  });
+
+// Portfolio
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.addEventListener("keydown", function(event) {
+
+      if (event.key === "3") {
+        const portfolioSection = document.getElementById("portfolio");
+        
+        if (portfolioSection) {
+          const portfolioTop = portfolioSection.getBoundingClientRect().top;
+          
+          window.scrollTo({
+            top: portfolioTop,
+            behavior: "smooth"
+          });
+        }
+      }
+    });
+  });
+
+// Contacto
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.addEventListener("keydown", function(event) {
+
+      if (event.key === "4") {
+        const contactSection = document.getElementById("contact");
+        
+        if (contactSection) {
+          const contactTop = contactSection.getBoundingClientRect().top;
+          
+          window.scrollTo({
+            top: contactTop,
+            behavior: "smooth"
+          });
+        }
+      }
+    });
+  });
+
+// Formulario contacto
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.addEventListener("keydown", function(event) {
+
+      if (event.key === "5") {
+        const containerFormContactSection = document.getElementById("containerFormContact");
+        
+        if (containerFormContact) {
+          const containerFormContactTop = containerFormContactSection.getBoundingClientRect().top;
+          
+          window.scrollTo({
+            top: containerFormContactTop,
             behavior: "smooth"
           });
         }
