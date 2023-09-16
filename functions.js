b9fdda3ffc52b50d4a10c81543d950be47483083
Edu <eduardoyauri@emmake.com>
2023-09-16 13:10:19 +0200

    Test Ok ok

--- functions.js
+++ functions.js
@@ -1,102 +1,41 @@
-// Who we are
+// Teclas 1 - 5 y a - b
 
 document.addEventListener("DOMContentLoaded", function() {
+    let currentSectionIndex = 0;
+    const sections = document.querySelectorAll("section");
+  
     document.addEventListener("keydown", function(event) {
-      
-      if (event.key === "1") {
-        
-        const whoweareSection = document.getElementById("whoweare");
-        
-        if (whoweareSection) {
-          
-          const whoweareTop = whoweareSection.getBoundingClientRect().top;
-          
-          window.scrollTo({
-            top: whoweareTop,
-            behavior: "smooth"
-          });
+      if (event.key === "a") {
+        currentSectionIndex = Math.max(0, currentSectionIndex - 1);
+      } else if (event.key === "b") {
+        currentSectionIndex = Math.min(sections.length - 1, currentSectionIndex + 1);
+      } else if (event.key >= "1" && event.key <= "5") {
+        const sectionId = event.key === "1" ? "whoweare" :
+                          event.key === "2" ? "service" :
+                          event.key === "3" ? "portfolio" :
+                          event.key === "4" ? "contact" :
+                          event.key === "5" ? "containerFormContact" : null;
+  
+        if (sectionId) {
+          const targetSection = document.getElementById(sectionId);
+  
+          if (targetSection) {
+            currentSectionIndex = Array.from(sections).indexOf(targetSection);
+          }
         }
+      }
+  
+      const targetSection = sections[currentSectionIndex];
+  
+      if (targetSection) {
+        const sectionTop = targetSection.offsetTop;
+  
+        window.scrollTo({
+          top: sectionTop,
+          behavior: "smooth"
+        });
       }
     });
   });
- 
-// Service
-
-document.addEventListener("DOMContentLoaded", function() {
-    document.addEventListener("keydown", function(event) {
-
-      if (event.key === "2") {
-        const serviceSection = document.getElementById("service");
-        
-        if (serviceSection) {
-          const serviceTop = serviceSection.getBoundingClientRect().top;
-          
-          window.scrollTo({
-            top: serviceTop,
-            behavior: "smooth"
-          });
-        }
-      }
-    });
-  });
-
-// Portfolio
-
-document.addEventListener("DOMContentLoaded", function() {
-    document.addEventListener("keydown", function(event) {
-
-      if (event.key === "3") {
-        const portfolioSection = document.getElementById("portfolio");
-        
-        if (portfolioSection) {
-          const portfolioTop = portfolioSection.getBoundingClientRect().top;
-          
-          window.scrollTo({
-            top: portfolioTop,
-            behavior: "smooth"
-          });
-        }
-      }
-    });
-  });
-
-// Contacto
-
-document.addEventListener("DOMContentLoaded", function() {
-    document.addEventListener("keydown", function(event) {
-
-      if (event.key === "4") {
-        const contactSection = document.getElementById("contact");
-        
-        if (contactSection) {
-          const contactTop = contactSection.getBoundingClientRect().top;
-          
-          window.scrollTo({
-            top: contactTop,
-            behavior: "smooth"
-          });
-        }
-      }
-    });
-  });
-
-// Formulario contacto
-
-document.addEventListener("DOMContentLoaded", function() {
-    document.addEventListener("keydown", function(event) {
-
-      if (event.key === "5") {
-        const containerFormContactSection = document.getElementById("containerFormContact");
-        
-        if (containerFormContactSection) {
-          const containerFormContactTop = containerFormContactSection.getBoundingClientRect().top;
-          
-          window.scrollTo({
-            top: containerFormContactTop,
-            behavior: "smooth"
-          });
-        }
-      }
-    });
-  });
+  
   
